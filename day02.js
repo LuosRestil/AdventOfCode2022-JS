bbb2516ee1963c582242f14ab34fc64bb7549fbe
Luos Restil <luosrestil@Luoss-MacBook-Pro.local>
2022-12-02 14:54:19 -0600

    optimize day02

--- day02.js
+++ day02.js
@@ -1,39 +1,43 @@
+console.time("process");
+
 const fs = require('fs');
 let input = fs.readFileSync('inputs/day02.txt', 'utf8').split('\n');
 
 // Part 1
 let score = 0;
+const beats = {'C': 'B', 'B': 'A', 'A': 'C'};
 for (let line of input) {
   const opponentMove = line[0];
   const myMove = String.fromCharCode(line.charCodeAt(2) - 23);
-  score += calculateMatchScore1(myMove, opponentMove);
+  score += calculateMatchScore1(myMove, opponentMove, beats);
 }
 console.log('Answer 1: ' + score);
 
 // Part 2
 score = 0;
+const requiredMoves = {
+  'A': {'X': 'C', 'Y': 'A', 'Z': 'B'},
+  'B': {'X': 'A', 'Y': 'B', 'Z': 'C'},
+  'C': {'X': 'B', 'Y': 'C', 'Z': 'A'}
+}
 for (let line of input) {
   const opponentMove = line[0];
   const requiredResult = line[2];
-  score += calculateMatchScore2(opponentMove, requiredResult);
+  score += calculateMatchScore2(opponentMove, requiredResult, requiredMoves);
 }
 console.log('Answer 2: ' + score);
 
+console.timeEnd("process");
 
-function calculateMatchScore1(myMove, opponentMove) {
-  const beats = {'C': 'B', 'B': 'A', 'A': 'C'};
-  const moveScore = myMove.charCodeAt(0) - 64;
+
+function calculateMatchScore1(myMove, opponentMove, beats) {
+  const moveScore = myMove.charCodeAt(0) - 64; // 64 is ascii value of 'A'
   let resultScore = 0 + (myMove === opponentMove ? 3 : beats[myMove] === opponentMove ? 6 : 0);
   return moveScore + resultScore;
 }
 
-function calculateMatchScore2(opponentMove, requiredResult) {
-  const resultScore = (requiredResult.charCodeAt(0) - 'X'.charCodeAt(0)) * 3;
-  const requiredMoves = {
-    'A': {'X': 'C', 'Y': 'A', 'Z': 'B'},
-    'B': {'X': 'A', 'Y': 'B', 'Z': 'C'},
-    'C': {'X': 'B', 'Y': 'C', 'Z': 'A'}
-  }
+function calculateMatchScore2(opponentMove, requiredResult, requiredMoves) {
+  const resultScore = (requiredResult.charCodeAt(0) - 88) * 3; // 88 is ascii value of 'X'
   const requiredMove = requiredMoves[opponentMove][requiredResult];
   const moveScore = requiredMove.charCodeAt(0) - 64;
   return resultScore + moveScore;
