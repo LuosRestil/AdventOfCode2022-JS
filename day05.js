a6306c892ad4e51a79ca0e9ca2e21127b4f3e3a2
Luos Restil <luosrestil@Luoss-MacBook-Pro.local>
2022-12-05 08:39:47 -0600

    day05 cleanup

--- day05.js
+++ day05.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 let input = fs.readFileSync('inputs/day05.txt', 'utf8').split('\n');
 
+// setup
 let crateLines = [];
 let instructions = [];
 let numStacks;
@@ -14,32 +15,38 @@ for (let line of input) {
   pushTo.push(line);
 }
 
-// create crate stacks as arrays
-let crates = new Array(numStacks).fill(null).map(_ => new Array());
-for (let i = crateLines.length - 1; i >= 0; i--) {
-  const line = crateLines[i];
-  for (let j = 0; j < line.length; j++) {
-    if (j % 4 === 1 && line[j] !== ' ') {
-      crates[Math.floor(j / 4)].push(line[j]);
+for (let i = 1; i <= 2; i++) {
+  // create crate stacks as arrays
+  let crates = new Array(numStacks).fill(null).map(_ => new Array());
+  for (let i = crateLines.length - 1; i >= 0; i--) {
+    const line = crateLines[i];
+    for (let j = 0; j < line.length; j++) {
+      if (j % 4 === 1 && line[j] !== ' ') {
+        crates[Math.floor(j / 4)].push(line[j]);
+      }
     }
   }
+
+  // follow instructions
+  for (let instruction of instructions) {
+    const nums = instruction.match(/[0-9]+/g)
+    const numCrates = parseInt(nums[0]);
+    const source = crates[parseInt(nums[1]) - 1];
+    const destination = crates[parseInt(nums[2]) - 1];
+    if (i === 1) {
+      // Part 1
+      for (let i = 0; i < numCrates; i++) {
+        destination.push(source.pop() || '');
+      }
+    } else {
+      // Part 2
+      for (let i = numCrates; i > 0; i--) {
+        destination.push(source[source.length - i]);
+      }
+      source.splice(source.length - numCrates);
+    }
+  }
+  let answer = crates.map(crate => crate.pop() || '').join('');
+  console.log(`Answer ${i}: ` + answer);
 }
 
-// follow instructions
-for (let instruction of instructions) {
-  const nums = instruction.match(/[0-9]+/g)
-  const numCrates = parseInt(nums[0]);
-  const sourceIndex = parseInt(nums[1]) - 1;
-  const destinationIndex = parseInt(nums[2]) - 1;
-  // Part 1
-  // for (let i = 0; i < numCrates; i++) {
-  //   crates[destinationIndex].push(crates[sourceIndex].pop() || '');
-  // }
-  // Part 2
-  for (let i = numCrates; i > 0; i--) {
-    crates[destinationIndex].push(crates[sourceIndex][crates[sourceIndex].length - i]);
-  }
-  crates[sourceIndex].splice(crates[sourceIndex].length - numCrates);
-}
-let answer = crates.map(crate => crate.pop() || '').join('');
-console.log("Answer: " + answer);
