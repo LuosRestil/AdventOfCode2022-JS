f4cccce2d36624c4fc80c53bb07707d1ea2e57ad
Luos Restil <luosrestil@Luoss-MacBook-Pro.local>
2022-12-21 14:02:49 -0600

    day17 pt1 no arrays

--- day17.js
+++ day17.js
@@ -3,102 +3,76 @@ const fs = require('fs');
 const jetPattern = fs.readFileSync('inputs/day17.txt', 'utf8').split('').map(char => char === '<' ? -1 : 1);
 const blocks = [
   {locs: [[0, 0], [0, 1], [0, 2], [0, 3]], height: 1},
-  {locs: [[0, 1], [-1, 0], [-1, 1], [-1, 2], [-2, 1]], height: 3},
-  {locs: [[0, 0], [0, 1], [0, 2], [-1, 2], [-2, 2]], height: 3},
-  {locs: [[0, 0], [-1, 0], [-2, 0], [-3, 0]], height: 4},
-  {locs: [[0, 0], [0, 1], [-1, 0], [-1, 1]], height: 2}
+  {locs: [[0, 1], [1, 0], [1, 1], [1, 2], [2, 1]], height: 3},
+  {locs: [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2]], height: 3},
+  {locs: [[0, 0], [1, 0], [2, 0], [3, 0]], height: 4},
+  {locs: [[0, 0], [0, 1], [1, 0], [1, 1]], height: 2}
 ];
-const cumulativeBlockHeight = blocks.reduce((height, block) => height + block.height, 0);
 let numRocks = 2022;
-let boardHeight = Math.ceil((numRocks / blocks.length) * cumulativeBlockHeight);
-let boardWidth = 7;
-let board = new Array(boardHeight).fill(null).map(() => new Array(boardWidth).fill(0));
-let highPoint = board.length - 1;
+const boardWidth = 7;
+let highPoint = 0;
+let floor = -1;
+
+let occupied = {};
+for (let i = 0; i < boardWidth; i++) {
+  occupied[i] = new Set();
+}
 
 // drop pieces
 let [blocksCounter, jetsCounter] = [0, 0];
 let [blocksLen, jetsLen] = [blocks.length, jetPattern.length];
 let blockSettled = true;
-let block, loc, move, jet;
+let block, loc, move, jet, floorSearch;
 while (blocksCounter < numRocks) {
   jet = jetPattern[jetsCounter % jetsLen];
   if (blockSettled) {
     // spawn new block
     block = blocks[blocksCounter % blocksLen];
-    loc = [highPoint - 3, 2];
+    loc = [highPoint + 3, 2];
     move = 'blow';
     blockSettled = false;
+    floorSearch = true;
   }
-
-  // drawPiece(block, loc);
-  // drawBoard();
-  // clearPiece();
-
   if (move === 'blow') {
     // move to the side
     if (canBeBlown(block, loc, jet)) {
       loc[1] += jet;
-    }
+    } 
     jetsCounter++;
     move = 'fall';
   } else {
     // fall
     if (canFall(block, loc)) {
-      loc[0]++;
+      loc[0]--;
     } else {
       settlePiece(block, loc);
-      // drawBoard();
       blockSettled = true;
-      highPoint = Math.min(loc[0] - block.height, highPoint);
+      highPoint = Math.max(loc[0] + block.height, highPoint);
       blocksCounter++;
-
     }
     move = 'blow';
   }
+
 }
-console.log('Answer 1: ' + (boardHeight - highPoint - 1));
+
+console.log('Answer 1: ' + highPoint);
 
 function canBeBlown(block, loc, jet) {
   return block.locs.every(square => {
     let proposedCol = loc[1] + square[1] + jet;
-    return proposedCol > -1 && proposedCol < boardWidth && board[loc[0] + square[0]][proposedCol] === 0;
+    return proposedCol > -1 && proposedCol < boardWidth && !occupied[proposedCol].has(loc[0] + square[0]);
   })
 }
 
 function canFall(block, loc) {
   return block.locs.every(square => {
-    let proposedRow = loc[0] + square[0] + 1;
-    return proposedRow < boardHeight && board[proposedRow][loc[1] + square[1]] !== 1;
+    let proposedRow = loc[0] + square[0] - 1;
+    return proposedRow > floor && !occupied[loc[1] + square[1]].has(proposedRow);
   })
 }
 
 function settlePiece(block, loc) {
   block.locs.forEach(square => {
-    board[loc[0] + square[0]][loc[1] + square[1]] = 1;
+    occupied[loc[1] + square[1]].add(loc[0] + square[0]);
   })
-}
-
-function drawPiece(block, loc) {
-  block.locs.forEach(square => {
-    board[loc[0] + square[0]][loc[1] + square[1]] = 2;
-  })
-}
-
-function clearPiece() {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      if (board[i][j] === 2) {
-        board[i][j] = 0;
-      }
-    }
-  }
-}
-
-function drawBoard() {
-  for (let line of board) {
-    console.log(JSON.stringify(line));
-  }
-  console.log();
-  console.log("*******************");
-  console.log();
 }
